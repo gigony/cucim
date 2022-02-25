a12c9e1f333ed69c19f92ab1e415aebbca1d6453
Gigon Bae <gbae@nvidia.com>
2022-02-24 17:22:53 -0800

    Add GDS benchmark scripts
    Signed-off-by: Gigon Bae <gbae@nvidia.com>

---
+++ benchmarks/gds/gen_testdata.py
@@ -0,0 +1,12 @@
+import secrets
+
+
+def gen_data(data_folder: str):
+    for file_size in (0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0):
+        with open(f"{data_folder}/data_{file_size}.blob", 'wb') as fp:
+            for i in range(int(file_size/0.1)):
+                fp.write(secrets.token_bytes(int(2**30 * 0.1)))
+
+
+if __name__ == "__main__":
+    gen_data(".")
